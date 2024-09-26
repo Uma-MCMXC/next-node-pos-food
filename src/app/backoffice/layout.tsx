a1e87a46aa4f95d8267292f85f683f3874df67c3
Uma-MCMXC <150512811+Uma-MCMXC@users.noreply.github.com>
2024-09-26 21:11:10 +0700

    edit svg

--- src/app/backoffice/layout.tsx
+++ src/app/backoffice/layout.tsx
@@ -84,9 +84,9 @@ export default function DashboardLayout({
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"
                   />
                 </svg>
